9e695387630b7b422d9ad2090bf7d8601789a23f
quge2020 <64964792+quge2020@users.noreply.github.com>
2020-05-09 11:16:45 +0800

    Add files via upload

---
+++ T/sdsmplsm/index.php
@@ -0,0 +1,55 @@
+﻿<html>
+<head>
+<meta name="viewport" content="width=device-width; initial-scale=1.0;  minimum-scale=1.0; maximum-scale=2.0"/>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312" /> 
+<DIV align=center>
+<title>扫到什么评论什么</title>
+<meta name="keywords" content="扫到什么评论什么"/>
+<meta name="description" content="扫到什么评论什么"/>
+<br><br><br><br><br><br><br><br><br><br><br>
+<img id="imgs">
+<script>
+var picARRAY=[
+'img/1.png',
+'img/2.png',
+'img/3.png',
+'img/4.png',
+'img/5.png',
+'img/6.png',
+'img/7.png',
+'img/8.png',
+'img/9.png',
+'img/10.png',
+'img/11.png',
+'img/12.png',
+'img/13.png',
+'img/14.png',
+'img/15.png',
+'img/16.png',
+'img/17.png',
+'img/18.png',
+'img/19.png',
+'img/20.png',
+'img/21.png',
+'img/22.png',
+'img/23.png',
+'img/24.png',
+'img/25.png',
+'img/26.png',
+'img/27.png',
+]
+var t=new Date().getTime().toString()
+var t2=parseInt(t.substr(t.length-1,t.length))%picARRAY.length
+document.getElementById("imgs").src=picARRAY[t2]
+</script>
+</div>
+
+<style>
+        #center-nav-demo {text-align: center;}
+        #center-nav-demo .nav {display: inline-block; margin: 0; padding: 0; border: 1px solid red;}
+        #center-nav-demo .nav li {display: inline-block; border: 1px solid blue;}
+    </style>
+	<div style="display:none;"><script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1262896806'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s19.cnzz.com/z_stat.php%3Fid%3D1262896806%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));</script></div> 
+</div>
+</head>
+ </html>
---
+++ T/sdsmplsm1/index.php
@@ -0,0 +1,33 @@
+﻿<html>
+<head>
+<body>
+<meta name="viewport" content="width=device-width; initial-scale=1.0;  minimum-scale=1.0; maximum-scale=2.0"/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<link href="css/index.css" rel="stylesheet" type="text/css" /> 
+<link href="ico.ico" rel="shortcut icon" />
+<title>扫到什么评论什么</title>
+<meta name="keywords" content="扫到什么评论什么"/>
+<meta name="description" content="扫到什么评论什么"/>
+<br><br><br><br><br><br><br><br><br><br><br>
+<style> 
+.divcss5{text-align:center} 
+</style> 
+<div class="divcss5">
+<font size=5>
+<?php
+ $txt=file('ab.txt');
+ $key=array_rand($txt);
+ echo $txt[$key];
+?>
+
+</font>
+</div>
+<style> 
+        #center-nav-demo {text-align: center;}
+        #center-nav-demo .nav {display: inline-block; margin: 0; padding: 0; border: 1px solid red;}
+        #center-nav-demo .nav li {display: inline-block; border: 1px solid blue;}
+    </style>
+<div style="display:none;"><script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1262896806'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s19.cnzz.com/z_stat.php%3Fid%3D1262896806%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));</script></div> 
+</body>
+</head>
+ </html>
